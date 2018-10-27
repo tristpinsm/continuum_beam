2f3222dc646bb319fdce32969cb71ce330b45e1e
Tristan Pinsonneault-Marotte <tristpinsm@phas.ubc.ca>
2018-10-26 18:11:53 -0700

    Added conditioning threshold to matrix inverse and started validating on simulated visibilities.

--- continuum_beam.py
+++ continuum_beam.py
@@ -7,16 +7,22 @@ from mat_prod import outer_sum
 
 class ModelVis(object):
     c = 2.99792458e2
+    synch_ind = -0.75
 
     def __init__(self, fname="./lambda_haslam408_nofilt.fits", freq=408.):
         # observed frequency
         self.freq = freq
         self.wl = self.c / freq
 
+        self.synch_ind = -0.75
+
         # smooth map using CHIME EW primary beam
         self.basemap = healpy.fitsfunc.read_map(fname)
         self.nside = int((self.basemap.shape[0]/12)**0.5)
         self.smoothmap = healpy.sphtfunc.smoothing(self.basemap, sigma=self._res())
+
+        # scale map with average synchrotron index
+        #self.smoothmap *= (freq / 408.)**self.synch_ind
 
         # get an observer at CHIME arguments
         self.obs = ephem.chime_observer().skyfield_obs()
@@ -34,25 +40,27 @@ class ModelVis(object):
         return np.sum(self._basis, axis=2)
 
 
-    def fit_beam(self, times, vis, weight, n, max_za=90., return_basis=False):
+    def fit_beam(self, times, vis, weight, n, max_za=90., rcond=1e-15):
         # generate model basis
         self._gen_basis(times, vis, n, max_za)
         # construct least squares equation
         # take the real part since we omit the lower half of the vis matrix
-        #M = np.sum(np.matmul(self._basis[:,:,:,np.newaxis], self._basis[:,:,np.newaxis,:]).real
-        #           * weight[:,:,np.newaxis,np.newaxis], axis=(0,1))
         M = np.zeros((n, n), dtype=np.float32)
+        v = np.zeros((n,), dtype=np.float32)
+        for t in range(vis.shape[1]):
+            M += np.dot(self._basis[:,t,:].T.conj() * weight[:,t],
+                        self._basis[:,t,:]).real
+            v += np.dot((vis * weight)[:,t].T, self._basis.conj()[:,t,:]).real
         #for i in range(n):
         #    for j in range(i, n):wgt_cal[:,time_slice]
         #        M[i,j] = np.sum((self._basis[:,:,i] * self._basis[:,:,j]).real
         #                        * weight[:,:,np.newaxis], axis=(0,1))
         #        M[j,i] = M[i,j]
-        M = outer_sum(self._basis, weight, M)
+        #M = outer_sum(self._basis, weight, M)
         self.M = M
-        v = np.sum(((vis * weight)[:,:,np.newaxis] * self._basis.conj()).real, axis=(0,1))
         self.v = v
         # return solution
-        return np.dot(np.linalg.inv(M), v)
+        return np.dot(np.linalg.pinv(M, rcond=rcond), v)
 
     def _gen_basis(self, times, vis, n, max_za=90.):
         # evaluate Haslam map at n declinations and all times
