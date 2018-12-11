d03d82d969459521f8020c57d91d34e381c2abce
Tristan Pinsonneault-Marotte <tristpinsm@phas.ubc.ca>
2018-12-11 10:36:04 -0800

    Added chi2, covariance, model visibilities.

--- continuum_beam.py
+++ continuum_beam.py
@@ -21,6 +21,7 @@ class ModelVis(object):
         self.basemap = healpy.fitsfunc.read_map(fname)
         self.nside = int((self.basemap.shape[0]/12)**0.5)
         self.smoothmap = healpy.sphtfunc.smoothing(self.basemap, sigma=self._res())
+        #self.smoothmap = self.basemap
 
         # scale map with average synchrotron index
         #self.smoothmap *= (freq / 408.)**self.synch_ind
@@ -35,13 +36,20 @@ class ModelVis(object):
             # generate them
             pass
 
-    def get_vis(self, times, vis, n, max_za=90., beam=1.):
+    def get_vis(self, times, vis, n, max_za=90., model_beam=None):
         # use this to check/visualize model
-        self._gen_basis(times, vis, n, max_za, beam)
-        return np.sum(self._basis, axis=2)
+        self._gen_basis(times, vis, n, max_za)
+        if model_beam is None:
+            beam = np.ones_like(self.za)
+        else:
+            beam = model_beam(self.za)
+        return np.sum(self._basis * beam, axis=2)
 
-    def fit_beam(self, times, vis, weight, n, max_za=90., rcond=1e-15,
-                 xtalk_iter=None):
+    def get_chi2(self, times, vis, weight, n, max_za=90.):
+        self.fit_beam(times, vis, weight, n, max_za)
+        return self.chi2
+
+    def fit_beam(self, times, vis, weight, n, max_za=90., rcond=None):
         if xtalk_iter is None:
             xtalk = 0.
             xtalk_iter = 1
@@ -54,32 +62,36 @@ class ModelVis(object):
             self._gen_basis(times, vis - xtalk, n, max_za)
             # construct least squares equation
             # take the real part since we omit the lower half of the vis matrix
-            M = np.zeros((n, n), dtype=np.float32)
-            v = np.zeros((n,), dtype=np.float32)
+            M = np.zeros((n, n), dtype=np.float64)
+            v = np.zeros((n,), dtype=np.float64)
             for t in range(vis.shape[1]):
                 M += np.dot(self._basis[:,t,:].T.conj() * weight[:,t],
                             self._basis[:,t,:]).real
                 v += np.dot((vis * weight)[:,t].T, self._basis.conj()[:,t,:]).real
-            #for i in range(n):
-            #    for j in range(i, n):wgt_cal[:,time_slice]
-            #        M[i,j] = np.sum((self._basis[:,:,i] * self._basis[:,:,j]).real
-            #                        * weight[:,:,np.newaxis], axis=(0,1))
-            #        M[j,i] = M[i,j]
-            #M = outer_sum(self._basis, weight, M)
-            res = np.dot(np.linalg.pinv(M, rcond=rcond), v)
-
+            # normalize to order unity
+            #norm = np.median(np.abs(v))
+            #v /= norm
+            #M /= norm
+            # invert to solve
+            if rcond is None:
+                self.beam_sol = np.dot(np.linalg.inv(M), v)
+            else:
+                self.beam_sol = np.dot(np.linalg.pinv(M, rcond=rcond), v)
             # update cross-talk estimate using fit result
             if xtalk_iter is not None:
-                xtalk = (vis - self.get_vis(times, vis, n, max_za, res)).median(axis=-1)
-
-        # Save intermediate products for debugging
+                xtalk = (vis - self.get_vis(times, vis, n, max_za, self.beam_sol)).median(axis=-1)
+        # save intermediate products for debugging
         self.M = M
         self.v = v
         self.xtalk = xtalk
-        # return solution
-        return res
+        # calculate chi^2
+        self.chi2 = np.sum(np.abs((np.dot(self._basis, self.beam_sol) - vis))**2 * weight)
+        # calculate covariance
+        U, S, V = np.linalg.svd(self.M, full_matrices=False)
+        self.cov = np.dot(V, np.dot(np.diag(1./S), V.T))
+        return self.beam_sol
 
-    def _gen_basis(self, times, vis, n, max_za=90., beam=1.):
+    def _gen_basis(self, times, vis, n, max_za=90.):
         # evaluate Haslam map at n declinations and all times
         za = np.linspace(-max_za, max_za, n)
         az = np.zeros_like(za)
